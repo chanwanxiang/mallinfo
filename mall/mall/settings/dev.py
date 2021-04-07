d683e56f04eecd6aef9620182379a8e864971a31
isMe <595366700@qq.com>
2021-04-07 22:10:19 +0800

    keep coding

--- mall/mall/settings/dev.py
+++ mall/mall/settings/dev.py
@@ -12,11 +12,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os,sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 查看项目导包路径
+print(sys.path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 注册用户子应用
+    'mall.apps.users',
 ]
 
 MIDDLEWARE = [
@@ -92,13 +96,60 @@ WSGI_APPLICATION = 'mall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# django默认sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# 配置mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        # 数据库引擎
+        'ENGINE': 'django.db.backends.mysql',
+        # 数据库主机
+        'HOST': '47.98.188.248',
+        # 数据库端口
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'NAME': 'meiduo'
+    },
 }
 
+# 配置redis
+CACHES = {
+    # 默认
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.98.188.248/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # session
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.98.188.248/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # 验证码
+    "verify_code": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.98.188.248/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+
+# session额外配置
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -136,4 +187,66 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# 指定加载静态文件路由前缀
 STATIC_URL = '/static/'
+
+# 配置静态文件加载路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# 配置工程日志
+LOGGING = {
+    'version': 1,
+    # 是否禁用已经存在的日志器
+    'disable_existing_loggers': False,
+    # 日志信息显示的格式
+    'formatters': {
+        'verbose': {
+            # 等级 时间 模块 行数 信息
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            # 等级 模块 行数 信息
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    # 对日志进行过滤
+    'filters': {
+        # django在debug模式下才输出日志
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    # 日志处理方法
+    'handlers': {
+        # 向终端中输出日志
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        # 向文件中输出日志
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            # 日志文件的位置
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/meiduo.log'),
+            # 单个日志最大容量 300M
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        },
+    },
+    # 日志器
+    'loggers': {
+        # 定义了一个名为django的日志器
+        'django': {
+            # 可以同时向终端与文件中输出日志
+            'handlers': ['console', 'file'],
+            # 是否继续传递日志信息
+            'propagate': True,
+            # 日志器接收的最低日志级别
+            'level': 'INFO',
+        },
+    }
+}
