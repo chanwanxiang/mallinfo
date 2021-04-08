74a6af6f875c3cc1f8343e1ad26202a8d2993bde
wxchan <595366700@qq.com>
2021-04-08 20:29:09 +0800

    keep coding

--- mall/mall/apps/users/views.py
+++ mall/mall/apps/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+# 类视图
+class RegisterView(View):
+    # 用户注册
+    
+    def get(self,request):
+        # 提供用户注册页面
+        return render(request,'register.html')
