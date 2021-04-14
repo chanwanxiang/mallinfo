29c5c6e60a20800d6d9d0f03b72fa8aaf025dda6
wxchan <595366700@qq.com>
2021-04-14 18:08:20 +0800

    keep coding

--- mall/mall/apps/users/urls.py
+++ mall/mall/apps/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # 用户注册 reverse(users:register) == '/register/'
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^sample/$', views.sample),
 ]
--- mall/mall/apps/users/views.py
+++ mall/mall/apps/users/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def sample(request):
+    return HttpResponse('SAMPLE')
 
 # 类视图
 class RegisterView(View):
     # 用户注册
-    
     def get(self,request):
         # 提供用户注册页面
         return render(request,'register.html')
 
-    def post(self, request):
-        # 实现用户注册业务逻辑
+    # def post(self, request):
+    #     # 实现用户注册业务逻辑
